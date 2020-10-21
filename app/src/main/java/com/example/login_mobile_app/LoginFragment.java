aa7a9ce084e7d267965a52995e918f59f7da863e
Christina Gerstner <christinalgerstner@gmail.com>
2020-10-21 13:00:37 -0500

    added layout & constraints

--- app/src/main/java/com/example/login_mobile_app/LoginFragment.java
+++ app/src/main/java/com/example/login_mobile_app/LoginFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -18,17 +18,5 @@ public class FirstFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
     }
 }
